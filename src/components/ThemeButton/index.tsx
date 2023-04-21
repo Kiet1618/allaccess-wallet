2e53a24efe8c1672c4c1a424999a0517ff4a66f9
Kiet1618 <ngokiet3207@gmail.com>
2023-04-21 12:10:11 +0700

    init

---
+++ src/components/ThemeButton/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Home = () => {
+
+    return (
+        <div className="App">
+            <button >Switch Theme</button>
+        </div>
+    );
+}
+
+export default Home;
