83ada58f29739f326339406b073170fe8055bdd4
Kiet1618 <ngokiet3207@gmail.com>
2023-04-21 12:39:42 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import { GlobalStyles } from "./styles/global.css";
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./styles/themes.css"
 import { Theme } from "./types/theme.type";
 import Router from "./router";
-
+import { redirect, useLocation } from "react-router-dom"
 const App = () => {
   const [theme, setTheme] = useState('light');
+
+
+
   const themeToggler = () => {
     theme === 'light' ? setTheme('dark') : setTheme('light')
   }
+
 
   return (
     <ThemeProvider theme={theme === 'light' ? lightTheme as Theme : darkTheme as Theme}>
       <>
         <GlobalStyles />
         <Router />
-        <div className="App">
+        <div>
           <button onClick={themeToggler}>Switch Theme</button>
         </div>
       </>
