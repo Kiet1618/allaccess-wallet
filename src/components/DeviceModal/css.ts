78790b59f90a5b5b06212a3b763c52b1f763cd2a
Khiem NV <nvkhiem2016@gmail.com>
2023-06-16 15:30:02 +0700

    move modal device

---
+++ src/components/DeviceModal/css.ts
@@ -0,0 +1,117 @@
+import styled from "styled-components";
+import { breakpoint } from "@app/utils";
+
+export const ContainerDevice = styled.div`
+  padding: 10px 10px;
+  border-radius: 8px;
+  border: 2px solid ${props => props.theme.colors.neutrals.gray200};
+  margin: 20px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ContainerDeviceModal = styled.div`
+  width: 100%;
+`;
+export const ListDevicesContainer = styled.div`
+  ${breakpoint("xs")`
+    margin-left: 10px;
+`}
+  ${breakpoint("lg")`
+    margin-left: 44px;
+  `}
+`;
+
+export const ContainerButtonFactors = styled.div`
+  width: max-content;
+  float: right;
+`;
+
+export const ContainerHeaderFactors = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+export const ContainerNumberFactors = styled.div`
+  padding: 12px 28px;
+  gap: 10px;
+  border-radius: 12px;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 20px;
+  background-color: #f0f0f1;
+`;
+
+export const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+  borderRadius: 4,
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
+  textAlign: "center",
+  alignItems: "center",
+};
+
+export const GroupLeftItemDevice = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ContainerText = styled.div`
+  margin-left: 10px;
+  text-align: left;
+`;
+
+export const NameText = styled.div`
+  text-transform: capitalize;
+  ${breakpoint("xs")`
+        font-weight: 400;
+        font-size: 13px;
+    `}
+  ${breakpoint("sm")`
+        font-weight: 600;
+        font-size: 16px;
+    `}
+`;
+
+export const SubTitlePage = styled.div`
+  font-weight: ${({ theme }) => theme.fontWeights.regular};
+  font-size: ${({ theme }) => theme.fontSizes.xs + "px"};
+  line-height: 24px;
+
+  color: ${({ theme }) => theme.colors.neutrals.gray600};
+  ${breakpoint("xs")`
+    width: 100%;
+`}
+  ${breakpoint("sm")`
+     margin: 20px 0;
+     width: 100%;
+  `}
+  ${breakpoint("md")`
+     margin: 20px 0;
+     width: 100%;
+  `}
+  ${breakpoint("lg")`
+     margin: 20px 0;
+     width: 60%;
+  `}
+`;
+
+export const IpText = styled.div`
+  font-weight: 400;
+
+  color: ${props => props.theme.colors.neutrals.gray500};
+  ${breakpoint("xs")`
+        font-size: 10px;
+    `}
+  ${breakpoint("sm")`
+        font-size: 12px;
+    `}
+`;
---
+++ src/components/DeviceModal/index.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { isEmpty } from "lodash";
+
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+
+import { Computer } from "@app/assets/icon";
+import { TitlePage } from "@app/styles";
+import { ShareInfo } from "@app/wallet/metadata";
+import { Button as CustomButton } from "@app/components";
+
+import { SubTitlePage, GroupLeftItemDevice, ContainerDeviceModal, ContainerButtonFactors, ContainerDevice, ContainerText, NameText, IpText, style } from "./css";
+
+type Props = {
+  device: ShareInfo;
+  loading: boolean;
+  handleClose: () => void;
+  handleConfirm: () => void;
+  isCurrent?: boolean;
+};
+const DeviceModal: React.FC<Props> = props => {
+  const { loading, device, handleClose, handleConfirm, isCurrent } = props;
+
+  return (
+    <Modal open={!isEmpty(device)} onClose={handleClose} aria-labelledby='modal-modal-title' aria-describedby='modal-modal-description'>
+      <Box sx={style}>
+        <TitlePage>Delete devices</TitlePage>
+        <SubTitlePage>This device will be erased and automatically logged out, are you sure?</SubTitlePage>
+        <ContainerDeviceModal>
+          <ContainerDevice>
+            <GroupLeftItemDevice>
+              <Computer />
+              <ContainerText>
+                <NameText>{`${device?.deviceInfo?.os} ${device?.deviceInfo?.version} ${isCurrent ? "(Current)" : ""}`}</NameText>
+                <IpText>IP: {device?.deviceInfo?.ipv4}</IpText>
+              </ContainerText>
+            </GroupLeftItemDevice>
+          </ContainerDevice>
+        </ContainerDeviceModal>
+        <ContainerButtonFactors>
+          <CustomButton onClick={handleClose} height='48px' width='150px' mTop='50px' mBottom='20px' mRight='20px' text='Back' styleButton='inactive'></CustomButton>
+          <CustomButton loading={loading} onClick={handleConfirm} height='48px' width='150px' mTop='50px' mBottom='20px' text="Yes, I'm sure" styleButton='primary'></CustomButton>
+        </ContainerButtonFactors>
+      </Box>
+    </Modal>
+  );
+};
+export default DeviceModal;
