e4c0a32ae1a8e3500889c24b51b83cb1a6da34fd
Khiem NV <nvkhiem2016@gmail.com>
2023-07-06 14:23:42 +0700

    add auto insert token fcm when user grant permission

--- src/hooks/usePushNotification.tsx
+++ src/hooks/usePushNotification.tsx
@@ -1,16 +1,32 @@
 import { useEffect, useState } from "react";
 import { getTokenFCM, onMessageListener } from "@app/configs/firebase"; // import from your firebase file
 import { MessagePayload } from "firebase/messaging"; // import from your firebase file
+import { useLocalStorage } from "usehooks-ts";
+import { KeyPair } from "@app/wallet/types";
+import { isEmpty } from "lodash";
+import { insertTokenByMasterPublicKey } from "@app/wallet/metadata";
 
 export function usePushNotifications(callback?: (_: MessagePayload) => void) {
   const [token, setToken] = useState("");
+  const [masterKey, _] = useLocalStorage<KeyPair | null>("master-key", null);
+
+  const handleGetTokenFCM = async () => {
+    const token = await getTokenFCM();
+    return token;
+  };
 
   useEffect(() => {
-    getTokenFCM()
-      .then(token => {
-        setToken(token);
-      })
-      .catch(error => console.error(error));
+    if (Notification.permission === "granted") {
+      handleGetTokenFCM().then(token => {
+        if (!isEmpty(masterKey)) {
+          insertTokenByMasterPublicKey({ token, masterPublicKey: masterKey.pubKey! });
+        }
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    handleGetTokenFCM().then(token => setToken(token));
   }, []);
 
   // Handle insert token by master public key
